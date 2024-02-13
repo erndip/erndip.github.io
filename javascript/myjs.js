c537c16b7e77245bf251c80903dc2bd50b9acc6f
erndip <erniedippold@gmail.com>
2024-02-13 02:23:39 -0600

    change everythin
    changed everything for valentines day

--- javascript/myjs.js
+++ javascript/myjs.js
@@ -1,96 +1,134 @@
-// JavaScript Document
-//button 2
-function grow1(elem){
-			let size = 24;
-			function growthFrame(elem){
-				//clearInterval(shrinkInterval)
-				if(size == 32){
-					clearInterval(growInterval)
-				}
-				else{
-					size += 1;
-					elem.style.fontSize = size + "px"
-				}
-			}	
-			var growInterval = setInterval(function() {growthFrame(elem)}, 40);
-		}
-		
-function shrink1(elem){
-			let size = 32;
-			function shrinkFrame(elem){
-				//clearInterval(growInterval)
-				if(size == 24){
-					clearInterval(shrinkInterval)
-				}
-				else{
-					size -= 1;
-					elem.style.fontSize = size + "px"
-				}
-			}
-	
-			var shrinkInterval = setInterval(function() {shrinkFrame(elem)}, 40);
-		}
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+let yesButton = document.getElementById("yes");
 
-//button 2
-function grow2(elem){
-			let size = 24;
-			function growthFrame(elem){
-				//clearInterval(shrinkInterval)
-				if(size == 32){
-					clearInterval(growInterval)
-				}
-				else{
-					size += 1;
-					elem.style.fontSize = size + "px"
-				}
-			}	
-			var growInterval = setInterval(function() {growthFrame(elem)}, 40);
-		}
-		
-function shrink2(elem){
-			let size = 32;
-			function shrinkFrame(elem){
-				//clearInterval(growInterval)
-				if(size == 24){
-					clearInterval(shrinkInterval)
-				}
-				else{
-					size -= 1;
-					elem.style.fontSize = size + "px"
-				}
-			}
-	
-			var shrinkInterval = setInterval(function() {shrinkFrame(elem)}, 40);
-		}
+let heartGrowing = false;
+let heartGrown = false;
+let heartFixing = false;
+let timeStart = 0;
 
-//button 3
-function grow3(elem){
-			let size = 24;
-			function growthFrame(elem){
-				//clearInterval(shrinkInterval)
-				if(size == 32){
-					clearInterval(growInterval)
-				}
-				else{
-					size += 1;
-					elem.style.fontSize = size + "px"
-				}
-			}	
-			var growInterval = setInterval(function() {growthFrame(elem)}, 40);
-		}
-		
-function shrink3(elem){
-			let size = 32;
-			function shrinkFrame(elem){
-				//clearInterval(growInterval)
-				if(size == 24){
-					clearInterval(shrinkInterval)
-				}
-				else{
-					size -= 1;
-					elem.style.fontSize = size + "px"
-				}
-			}
-	
-			var shrinkInterval = setInterval(function() {shrinkFrame(elem)}, 40);
-		}
+yesButton.onclick = function(){
+	heartGrowing = true;
+	timeStart = Date.now();
+	console.log(timeStart);
+}
+
+function drawLeftHeart(angle, color, strokeColor){
+	context.save();
+	context.lineJoin = "round";
+	context.lineWidth = "3";
+	context.fillStyle = color;
+	context.strokeStyle = strokeColor;
+	context.rotate(angle);
+	context.beginPath();
+	context.moveTo(0,0);
+	context.lineTo(0,-100);
+	context.arc(-50,-100,50,0,Math.PI, true);
+	context.lineTo(-90,-90);
+	context.lineTo(-90,-70);
+	context.lineTo(-70,-70);
+	context.lineTo(-70,-50);
+	context.lineTo(-50,-50);
+	context.lineTo(-50,-30);
+	context.lineTo(-30,-30);
+	context.lineTo(-30,-10);
+	context.lineTo(-10,-10);
+	context.closePath();
+
+	context.fill();
+	context.stroke();
+	context.restore();
+}
+
+function drawRightHeart(angle, color, strokeColor){
+	context.save();
+	context.lineJoin = "round";
+	context.lineWidth = "3";
+	context.fillStyle = color;
+	context.strokeStyle = strokeColor;
+	context.rotate(angle);
+	context.beginPath();
+	context.moveTo(0,0);
+	context.lineTo(0,-100);
+	context.arc(50,-100,50,-Math.PI,0);
+	context.lineTo(90,-90);
+	context.lineTo(70,-90);
+	context.lineTo(70,-70);
+	context.lineTo(50,-70);
+	context.lineTo(50,-50);
+	context.lineTo(30,-50);
+	context.lineTo(30,-30);
+	context.lineTo(10,-30);
+	context.lineTo(10,-10);
+	context.closePath();
+	context.fill();
+	context.stroke();
+	context.restore();
+}
+
+function drawBrokenHeart(){
+	drawLeftHeart(Math.PI/2, "grey", "black");
+	drawRightHeart(-Math.PI/2, "grey", "black");
+}
+// pink: rgb(255,192,203)
+function growHeart(){
+	let time = (Date.now()-timeStart)/2000;
+	context.clearRect(0,0,canvas.width,canvas.height);
+	let colorPercent = time/(Math.PI/4);
+	let color = `rgb(${128+(127*colorPercent)}, ${128+((192-128)*colorPercent)}, ${128+((203-128)*colorPercent)})`
+
+
+	drawLeftHeart((Math.PI/2)-time, color, "black");
+	drawRightHeart((-Math.PI/2)+time, color, "black");
+	if (time >= Math.PI/4){
+		heartGrowing = false;
+		heartGrown = true;
+	}
+}
+
+function fixHeart(){
+	let time = (Date.now()-timeStart)/2000;
+
+}
+
+function drawFullHeart(){
+	context.save();
+	context.lineWidth = "3";
+	context.fillStyle = "pink"
+	context.beginPath();
+	context.save();
+	context.rotate(Math.PI/4);
+	context.arc(-50,-100,50,-Math.PI, 0);
+	context.restore();
+	context.lineTo(0,0);
+	context.save();
+	context.rotate(-Math.PI/4);
+	context.arc(50,-100,50,-Math.PI, 0);
+	context.restore();
+	context.closePath();
+	context.fill();
+	context.stroke();
+	context.restore();
+}
+
+
+
+drawBrokenHeart();
+
+function draw(ts){
+	context.clearRect(0,0,canvas.width,canvas.height);
+	context.save();
+	context.translate(200,200);
+
+	if(heartGrowing){
+		growHeart(ts);
+	}
+	else if (heartGrown){
+		drawFullHeart();
+	}
+
+	context.restore();
+	window.requestAnimationFrame(draw);
+}
+window.requestAnimationFrame(draw);
+
