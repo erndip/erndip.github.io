dd31e2abfc1e36f164d4f31a16c37afd582bfa4f
erndip <erniedippold@gmail.com>
2024-02-13 03:09:09 -0600

    update 1

--- javascript/myjs.js
+++ javascript/myjs.js
@@ -1,5 +1,5 @@
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext("2d");
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
 let yesButton = document.getElementById("yes");
 
 let heartGrowing = false;
@@ -9,8 +9,10 @@ let timeStart = 0;
 
 yesButton.onclick = function(){
 	heartGrowing = true;
+	heartGrown = false;
+	heartFixing = false;
+
 	timeStart = Date.now();
-	console.log(timeStart);
 }
 
 function drawLeftHeart(angle, color, strokeColor){
@@ -72,7 +74,7 @@ function drawBrokenHeart(){
 }
 // pink: rgb(255,192,203)
 function growHeart(){
-	let time = (Date.now()-timeStart)/2000;
+	let time = (Date.now()-timeStart)**1.3/8000;
 	context.clearRect(0,0,canvas.width,canvas.height);
 	let colorPercent = time/(Math.PI/4);
 	let color = `rgb(${128+(127*colorPercent)}, ${128+((192-128)*colorPercent)}, ${128+((203-128)*colorPercent)})`
@@ -83,17 +85,55 @@ function growHeart(){
 	if (time >= Math.PI/4){
 		heartGrowing = false;
 		heartGrown = true;
+		heartFixing = true;
+		timeStart = Date.now();
 	}
 }
 
 function fixHeart(){
-	let time = (Date.now()-timeStart)/2000;
+	let time = (Date.now()-timeStart)**0.8/200;
+	let timeLimit = 2;
+	let perc = time/timeLimit;
+	context.save();
+	context.lineWidth = "3";
+	context.strokeStyle = `rgba(0,0,0,${0.8-perc})`
+	context.moveTo(0,0);
+	context.rotate(-Math.PI/4);
+	context.lineTo(10,-10);
+	context.lineTo(10,-30);
+	context.lineTo(30,-30);
+	context.lineTo(30,-50);
+	context.lineTo(50,-50);
+	context.lineTo(50,-70);
+	context.lineTo(70,-70);
+	context.lineTo(70,-90);
+	context.lineTo(90,-90);
+	context.lineTo(100,-100);
+
+	context.stroke();
+	context.restore();
+
+	// Draw circle
+	context.save();
+	context.globalCompositeOperation = "destnation-atop";
+	context.fillStyle = `rgba(255,0,0,${1-perc})`
+	context.beginPath();
+	context.arc(0,-75,perc*150,0,Math.PI*2);
+	context.closePath();
+	context.fill();
+	context.restore();
+
+
+	if (time >= timeLimit){
+		heartFixing = false;
+	}
 
 }
 
 function drawFullHeart(){
 	context.save();
 	context.lineWidth = "3";
+	context.strokeStyle = "black";
 	context.fillStyle = "pink"
 	context.beginPath();
 	context.save();
@@ -125,6 +165,9 @@ function draw(ts){
 	}
 	else if (heartGrown){
 		drawFullHeart();
+	}
+	if (heartFixing){
+		fixHeart();
 	}
 
 	context.restore();
